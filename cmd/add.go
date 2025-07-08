bc20a38150b6b6faf97cf0d8776c4a2ad2cc393a
Vipul Bhasin <vipulbhasin22@gmail.com>
2025-07-09 00:39:07 +0530

    Add the add command

---
+++ cmd/add.go
@@ -0,0 +1,37 @@
+/*
+Copyright © 2025 Vipul B
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// addCmd represents the add command
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "Add a new todo",
+	Long: `Add will create a new todo item to the list`,
+	Run: addRun,
+}
+
+func addRun(cmd *cobra.Command, args []string) {
+	fmt.Println("add called")
+}
+
+func init() {
+	rootCmd.AddCommand(addCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// addCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
