d712afb76bf9776ec16d506e1ec7f24b033b5f21
Vipul Bhasin <vipulbhasin22@gmail.com>
2025-07-10 11:30:18 +0530

    Pretty print priorities

--- cmd/list.go
+++ cmd/list.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
 	"text/tabwriter"
 
 	"github.com/spf13/cobra"
@@ -31,7 +30,7 @@ func listRun(cmd *cobra.Command, args []string) {
 	// fmt.Println(items)
 	w := tabwriter.NewWriter(os.Stdout, 3, 0, 1, ' ', 0)
 	for _, i := range items {
-		fmt.Fprintln(w, strconv.Itoa(i.Priority)+"\t"+i.Text+"\t")
+		fmt.Fprintln(w, i.PrettyP()+"\t"+i.Text+"\t")
 	}
 	w.Flush()
 }
--- todo/todo.go
+++ todo/todo.go
@@ -47,3 +47,13 @@ func (i *Item) SetPriority(pri int) {
 		i.Priority = 2
 	}
 }
+
+func (i *Item) PrettyP() string {
+	if i.Priority == 1 {
+		return "(1)"
+	}
+	if i.Priority == 3 {
+		return "(3)"
+	}
+	return " "
+}
