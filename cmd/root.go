f056003d401ca32cc329165ffbcd32ed2204330a
Vipul Bhasin <vipulbhasin22@gmail.com>
2025-07-15 10:47:55 +0530

    Add prefix for ENV vars

--- cmd/root.go
+++ cmd/root.go
@@ -41,6 +41,7 @@ func initConfig() {
 	viper.SetConfigName(".tri")  // name of config file (without extension)
 	viper.AddConfigPath("$HOME") // adding home directory as first search path
 	viper.AutomaticEnv()         // read in environment variables that match
+	viper.SetEnvPrefix("tri")    // set prefix for environment variables
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
